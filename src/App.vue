63f436c1b16523ccea25e14821e4d41691774d98
王小康 <wangxiaokang1@jd.com>
2022-01-15 15:17:21 +0800

    test自动部署

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <h1>测试前端一键部署</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
